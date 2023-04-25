b619dfd0e1a923dc447dea885c591ffc1f45cb5d
Suraj Pratap Singh <surajpratap27.org@gmail.com>
2023-04-25 17:47:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SlotMatch from "./SlotMatch";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="heading_style">🎰 Welcome to <span style={{ fontWeight: "bold" }}> Slot Machine game </span> 🎰</h1>
+      <div className="slotMachine">
+        <SlotMatch x='😄' y='😄' z='😄' />
+        <SlotMatch x='😄' y='🤯' z='😄' />
+        <SlotMatch x='😂' y='😒' z='😄' />
+        <SlotMatch x='😘' y='😘' z='😘' />
+      </div>
+
+    </>
+  )
 }
 
 export default App;
---
+++ src/SlotMatch.js
@@ -0,0 +1,29 @@
+import React from "react";
+const SlotMatch = (props) => {
+    let { x, y, z } = props;
+
+    if ((props.x === props.y) && (props.y === props.z)) {
+        return (
+            <>
+                <div className="slot_inner">
+                    <h1>{x} {y} {z}</h1>
+                    <h1>This is Matching.!</h1>
+                    <hr />
+                </div>
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <div className="slot_inner">
+                    <h1>{x} {y} {z}</h1>
+                    <h1>This is Not Matching.!</h1>
+                    <hr />
+                </div>
+            </>
+        )
+    }
+}
+
+export default SlotMatch;
